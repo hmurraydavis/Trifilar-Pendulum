1580db83ce2d3c293e083509196813557f1df273
Halie <hmurraydavis@users.noreply.github.com>
2013-12-15 23:46:14 -0500

    Reads the photo-resistor.

---
+++ Photoresistor_sensor.ino
@@ -0,0 +1,17 @@
+//declare global variables:
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+//setup loop:
+void setup() {
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  Serial.println("Hi there, little friend!");  
+  Serial.println(sensorValue);                 
+}
