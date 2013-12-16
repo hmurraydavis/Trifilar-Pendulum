4349276323a32ad877368a8dab09a6daeb25f392
Halie <hmurraydavis@users.noreply.github.com>
2013-12-15 23:54:49 -0500

    Millis doesn't work. :(

--- Photoresistor_sensor.ino
+++ Photoresistor_sensor.ino
@@ -1,6 +1,22 @@
+String voltagesign(float motor){
+    String sign;   
+  
+   if (motor>100){
+       sign="Nope!";
+       return sign;
+   }
+    
+    if (motor<=100){
+       sign="Gate happened";
+       return sign;
+   }
+}
+
+
 //declare global variables:
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
+
 
 //setup loop:
 void setup() {
@@ -12,6 +28,13 @@ void setup() {
 void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
-  Serial.println("Hi there, little friend!");  
-  Serial.println(sensorValue);                 
+  //Serial.println("Hi there, little friend!");  
+  Serial.println(sensorValue);
+  String gate=voltagesign(sensorValue);
+  Serial.println(gate);
+  if (gate=="Gate happened"){
+    Serial.print("The time of this period event was:");
+    float millis;
+    Serial.println(millis);
+  }
 }
